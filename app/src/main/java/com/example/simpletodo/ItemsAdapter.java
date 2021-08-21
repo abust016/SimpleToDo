7dc8214eb3414cd398ca640e39f7ab920eeb76af
Adrian Bustos <adrian@MacBook-Air.local>
2021-08-20 23:02:29 -0400

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -0,0 +1,63 @@
+package com.example.simpletodo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+//Responsible for displaying data from the model into row in the recycler view
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
+
+    List<String> items;
+    public ItemsAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Use layout inflator to inflate a view
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);//parameters
+
+        //wrap it inside a view wholder and return it
+
+        return new ViewHolder(todoView);
+    }
+
+    // responsible for binding data to a particular viewholder
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    //Grab the i tems at the position
+        String item = items.get(position);
+    //bind the item into the specfied viewholder
+        holder.bind(item);
+    }
+
+    //tells the recyclerView how many items are on the list
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    //container to provide easy access to views that represent each row of the list
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        //update the view inside of the viewholder with this data of string item
+        public void bind(String item) {
+            tvItem.setText(item);
+        }
+    }
+}
