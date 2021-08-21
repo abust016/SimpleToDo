c012112140b8b212a55e1900dfa3ab9303660816
abust016 <abust016@fiu.edu>
2021-08-21 15:04:03 -0400

    implemented mutation actions (add and remove) and implemented persistence (enabled loading model from file system and store to file system after changes)

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.simpletodo;
 
+import static org.apache.commons.io.FileUtils.readLines;
+import static org.apache.commons.io.FileUtils.writeLines;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,17 +14,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-   List<String> items;
+    List<String> items;
 
-Button btnAdd;
-EditText etItem;
-RecyclerView rvItems;
-ItemsAdapter itemsAdapter;
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
+    ItemsAdapter itemsAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,15 +41,16 @@ ItemsAdapter itemsAdapter;
         //etItem.setText("I'm doing this from Java");
 
 
-        items = new ArrayList<>();
-        items.add("Buy milk!");
-        items.add("Go to the gym!");
-        items.add("Have fun!");
+        //items = new ArrayList<>();
+        loadItems();
+        //items.add("Buy milk!");
+        //items.add("Go to the gym!");
+        //items.add("Have fun!");
 
-       ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
+        ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
             @Override
             public void onItemLongClicked(int position) {
-            //Delete the item from the model
+                //Delete the item from the model
                 items.remove(position);
                 //Notify the adapter at which position we deleted the item
                 itemsAdapter.notifyItemRemoved(position);
@@ -61,13 +70,36 @@ ItemsAdapter itemsAdapter;
                 // Add item to the model
                 items.add(todoItem);
                 //notify adapter that item has been inserted
-                itemsAdapter.notifyItemInserted(items.size()-1);
+                itemsAdapter.notifyItemInserted(items.size() - 1);
                 etItem.setText("");
                 Toast.makeText(getApplicationContext(), "item was added", Toast.LENGTH_SHORT).show();
+                saveItems();
             }
-
-
-
         });
     }
+
+    private File getDataFile() {
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    //this function will load items by reading every line of the data file
+    private void loadItems() {
+        try {
+            items = new ArrayList<>(readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("Main Activity", "Error reading items", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    //this function saves by writing them into the data file
+    private void saveItems() {
+        try {
+            writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing items", e);
+
+        }
+    }
+
 }
